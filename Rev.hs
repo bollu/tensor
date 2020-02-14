10283170d9e2b2b560ddea76af9b5e208280953a
Siddharth Bhat <siddu.druid@gmail.com>
2020-02-15 03:18:26 +0530

    experiment

--- Rev.hs
+++ Rev.hs
@@ -1,31 +1,206 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE BangPatterns #-}
+
 module Rev where
+import qualified Control.Monad.Fail as MF
 import Control.Monad
-data State s a = State { runstate :: s -> (a, s) } deriving (Functor)
+import Control.Monad.Fix
+import qualified Data.Map.Strict as M
+import Data.List
+import Control.Applicative
 
-instance Monad (State s) where
-  return x = State $ \s -> (x, s)
-  sa >>= a2sb = State $ \s -> let (a, s') = runstate sa s; sb = a2sb a; (b, s'') = runstate sb s' in (b, s'')
+data S s a = S { runs :: s -> (a, s) } deriving(Functor)
+data R s a = R { runr :: s -> (a, s) } deriving (Functor)
 
-instance Applicative (State s) where
-  pure = return
-  (<*>) = ap
+gets :: S s s
+gets = S $ \s -> (s, s)
 
-data Tardis s a = Tardis { runtardy :: s -> (a, s) } deriving(Functor)
+puts :: s -> S s ()
+puts s = S $ \_ -> ((), s)
 
-
-instance Monad (Tardis s) where
-  return x = Tardis $ \s -> (x, s)
-  sa >>= a2sb = Tardis $ \s -> let (a, s') = runtardy sa s; sb = a2sb a; (b, s'') = runtardy sb s' in (b, s'')
-
-instance Applicative (Tardis s) where
-  pure = return
-  (<*>) = ap
-
-newtype Ldif a = Ld (a->(a,a))
-
-lCnst c = Ld (\z -> (c, 0.0))
-lDvar x = Ld (\z -> (x, z))
+modifys :: (s -> s) -> S s ()
+modifys f = S $ \s -> ((), f s)
 
 
-llift f f' (Ld pp) =Ld (\n->let (p,pb)= pp eb;eb= (f' p)*n in (f p,pb))
+getr :: R r r
+getr = R $ \r -> (r, r)
+
+putr :: r -> R r ()
+putr r = R $ \_ -> ((), r)
+
+modifyr :: (r -> r) -> R r ()
+modifyr f = R $ \r -> ((), f r)
+
+instance Monad (S s) where
+ return x = S $ \s -> (x, s)
+ sx >>= x2sy = S $ \s -> let (x, s') = runs sx s; sy = x2sy x; (y, s'') = runs sy s' in (y, s'') 
+
+instance Applicative (S s) where pure = return; (<*>) = ap
+
+instance Monad (R s) where
+ return x = R $ \s -> (x, s)
+ sx >>= x2sy = R $ \s -> let (x, s'') = runr sx s'; sy = x2sy x; (y, s') = runr sy s in (y, s'') 
+
+instance Applicative (R s) where pure = return; (<*>) = ap
+
+type Name = String
+type Env a = M.Map Name a
+
+-- | instruction. build reverse mode AD
+{-
+data I a = I (S Env a) (R Env a) deriving(Functor)
+
+instance Monad I where
+  return x = I (return x) (return x)
+  (I sx rx) >>= x2syry = I is ir where
+    is = S $ \s -> let (x, s') = runs sx s; (I sy _) = x2syry x; (y, s'') = runs sy s' in (y, s'')
+    ir = R $ \r -> let (x, r') = runr rx r; (I _ ry) = x2syry x; (y, r'') = runr ry r' in (y, r'')
+instance Applicative I where pure = return; (<*>) = ap
+     
+getenv :: I (Env  Int)
+getenv = I (S $ \s -> (s, s)) (R $ (\r -> (r, r)))
+
+
+eval :: E -> I Int
+eval (EC i) = return i
+eval (EV n) = do
+  env <- getenv
+  return $ env M.! n
+-}
+
+-- x = y + z
+-- ds/dx = ds/dy.dy/dx + ds/dz.dz/dx
+
+data SRM a = SRM {
+  run :: Env Int -> Env Int -> (a, Env Int, Env Int)
+} 
+
+instance Functor SRM where
+  fmap f mx = do
+   x <- mx
+   return $ f x
+
+instance Monad SRM where
+  return a = SRM $ \v g -> (a, v, g)
+  sx >>= x2sy = SRM $ \v g  -> 
+    let ~(x, v', g'') = run sx v g'; sy = x2sy x; ~(y, v'', g') = run sy v' g in (y, v'', g'')
+
+instance MonadFix SRM where
+   mfix f = do
+    rec x <- (f x)
+    return x
+
+
+instance Applicative SRM where pure = return; (<*>) = ap
+
+accumgrad_ :: Name -> Int -> SRM ()
+accumgrad_ n v = SRM $ \vals ders -> ((), vals, M.insertWith (+) n v ders)
+
+recvgrad :: Name -> SRM Int
+-- recvgrad n = SRM $ \vals ders -> (M.findWithDefault 0 n ders, vals, ders)
+recvgrad n = SRM $ \vals ders -> (ders M.! n, vals, ders)
+
+recvval :: Name -> SRM Int
+recvval n = SRM $ \vals ders -> (vals M.! n, vals, ders)
+
+sendval :: Name -> Int -> SRM ()
+sendval n v = SRM $  \vals ders -> ((), M.insert n v vals, ders) 
+
+-- | values
+data V = VC Int | VN Name deriving(Show)
+
+evalV ::  V -> SRM Int
+evalV (VC n) = return n
+evalV (VN n) = recvval n
+
+accumgrad :: V -> Int -> SRM ()
+accumgrad (VC _) g = return ()
+accumgrad (VN n) g = accumgrad_ n g
+
+add :: Name -> V -> V -> SRM ()
+add n l r = do
+  il <- evalV l
+  ir <- evalV r
+
+  sendval n (il + ir)
+  -- n = l + r
+  -- ds/dn
+  dsdn <- recvgrad n
+  -- ds/dl = ds/dn . dn/dl ||| ds/dr = ds/dn . dn/dr
+  accumgrad l (dsdn*1)
+  accumgrad r (dsdn*1)
+
+var :: Name -> Int -> SRM ()
+var n val = mdo
+  sendval n val
+  return ()
+
+mul :: Name -> V -> V -> SRM ()
+mul n l r = mdo
+  -- n = l + r
+  -- ds/dn
+  dsdn <- recvgrad n
+
+  il <- evalV l
+  ir <- evalV r
+
+  sendval n (il + ir)
+
+  -- ds/dl = ds/dn . dn/dl ||| ds/dr = ds/dn . dn/dr
+  accumgrad l (dsdn*ir)
+  accumgrad r (dsdn*il)
+
+  return ()
+
+programsendrecv :: SRM ()
+programsendrecv = do
+  dz <- recvgrad "z"
+  sendval "z" dz
+
+program0 :: SRM ()
+program0 = var "z" 3
+
+program1 :: SRM ()
+program1 = 
+ var "x" 2 >> mul "xsq" (VN "x") (VN "x")
+
+program1' :: SRM Int
+program1' = mdo
+ -- compute x
+ sendval "x" 2
+ -- compute xsq
+ xv <- recvval "x"
+ sendval "xsq" $ xv * xv
+ -- accumulate derivatives
+
+ xsqval <- recvval "xsq"
+ accumgrad_ "x" (xv * ds_dxsq)
+ -- accumgrad_ "x" (xv * ds_dxsq)
+ ds_dxsq <- recvgrad "xsq"
+ accumgrad_ "xsq" 1
+ return ds_dxsq
+
+-- | contains x, ds_dx, sq, ds_dsq
+-- sq = x * x
+programflat :: (Int, Int, Int, Int)
+programflat = (x, ds_dx, sq, ds_dsq) where
+  x = 2
+  sq = x * x
+  ds_dsq = 1
+  dsq_dl = ds_dsq * x
+  dsq_dr = ds_dsq * x
+  ds_dx = dsq_dl + dsq_dr
+
+program2 :: SRM ()
+program2 = do
+ var "x" 2
+ var "y" 3
+ mul "xsq" (VN "x") (VN "x")
+ add "z" (VN "xsq") (VN "y")
+
+runprogram :: SRM a -> Name -> (a, Env Int, Env Int)
+runprogram p output = 
+  let (a, vals, ders) = run (do accumgrad_ output 1; v <- p; accumgrad_ output 1; return v) M.empty M.empty
+  in (a, vals, ders)
+
--- Tensor.hs
+++ Tensor.hs
@@ -9,7 +9,6 @@ data V a = V a | VS String -- values: either concrete  | symbolic
 deriving instance Functor V 
 type I = V Int -- indexes are either concrete or symbolic values.
 
-
 data T a where
   TAdd :: T a -> T a -> T a
   TMul :: T a -> T a -> T a
@@ -20,23 +19,20 @@ data T a where
   TEQ :: Show a => T a -> T a -> T Bool -- compare equal
   TITE :: T Bool -> T a -> T a -> T a -- if then else
 
-(.<) :: Show a => T a -> T a -> T Bool
-(.<) = TLT
+var :: String -> V a; var = VS
+tconst :: a -> T a; tconst = TV . V
+tsym :: String -> T a; tsym = TV . var
 
-(.==) :: Show a => T a -> T a -> T Bool
-(.==) = TEQ
-
-(.!) :: T a -> [I] -> T a
-(.!) = TIx
+(.<) :: Show a => T a -> T a -> T Bool; (.<) = TLT
+(.==) :: Show a => T a -> T a -> T Bool; (.==) = TEQ
+(.!) :: T a -> [I] -> T a; (.!) = TIx
 
 
 -- pred .? x .: y
 -- can setup precedences so this parses as
 -- (.:) (.? pred x) y
-(.:) :: (T a -> T a) -> T a -> T a
-(.:) predx y = predx y
-(.?) :: T Bool -> T a -> T a -> T a
-(.?) = TITE
+(.:) :: (T a -> T a) -> T a -> T a; (.:) predx y = predx y
+(.?) :: T Bool -> T a -> T a -> T a; (.?) = TITE
 
 instance Num a => Num (T a) where
   (+) = TAdd
@@ -52,7 +48,7 @@ data M a where
  MR :: a -> M a -- return
  MF :: (a -> b) -> M a -> M b -- fmap
  MS :: (T Float -> M a) -> M a -- sample
- MD :: M a -> M a -> (T a -> M b) -> M b -- derivative
+ MD :: Num a => M a -> M a -> (T a -> M b) -> M b -- derivative
 
 instance Functor M where
  fmap = MF
@@ -68,34 +64,44 @@ instance Monad M where
 -- | compile monad
 data CM a = CM { runcm :: Int -> (Int, a) }
 
-cmuniq :: CM Int
-cmuniq = CM $ \i -> (i+1,i)
+cmuniq :: CM Int; cmuniq = CM $ \i -> (i+1,i)
 
-runcompiler :: CM a -> a
-runcompiler cm = let (_, a) = runcm cm 0 in a
+runcompiler :: CM a -> a; runcompiler cm = let (_, a) = runcm cm 0 in a
 
 instance Functor CM where
   fmap f (CM cm) = CM $ \i -> let (i', a) = cm i in (i', f a)
 instance Applicative CM where
-  pure = return
-  (<*>) = ap
+  pure = return; (<*>) = ap
 instance Monad CM where
   return x = CM $ \i -> (i, x)
-  (CM cm) >>= f = CM $ \i -> let (i', a) = cm i; (i'', b) = runcm (f a) i' in (i'', b)
+  (CM cm) >>= f = CM $ \i -> 
+    let (i', a) = cm i; (i'', b) = runcm (f a) i' in (i'', b)
 
-compileder :: T a -> T a -> CM (T a)
-compileder = undefined
+-- | TODO: create a new class called "DC a", which means one has
+-- a differential calculus on that type. eg. finite differences.
+compileder :: Num a => T a -> T a -> CM (T a)
+compileder d@(TIx (TV (VS name)) ixs) e = return $ case e of
+ TAdd l r -> TAdd (compileder d l) (compileder d r)
+ TMul l r -> let l' = compileder d l; r' = compileder d r
+    in TAdd (TMul l r') (TMul r' l)
+ TIx (TV (VS name')) ixs' -> error "not done"
+ TV _ -> 0
+ TContract ixs t' -> TContract ixs (compileder d t')
+ TLT l r ->  0 -- | good question, WTF?
+ TEQ l r -> 0 -- | WTF?
+ TITE l r -> 0 -- | WTF?
+ _ -> 0 -- anything else, it's a straight 0
+compileder (TV (V _)) _ = 0
+compileder n d = error $ "unknown derivative"
 
 -- | compile the monad into a large expression
 compileM :: M a -> CM (T a)
 compileM (MR t) = return (TV $ V t)
 compileM (MS fk) = do
-  i <- cmuniq
-  let v = TV (VS (show i))
+  i <- cmuniq; let v = TV (VS (show i))
   compileM $ fk v
 compileM (MD n d derk) = do
-  n <- compileM n
-  d <- compileM d
+  n <- compileM n; d <- compileM d
   der <- compileder n d
   compileM $ derk der
 
